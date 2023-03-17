4310a5ca38902cfa8d4044e0efd84e03293d925c
kenkairon <ingeinfo2006@gmail.com>
2023-03-17 16:47:28 -0300

    Segunda version estatica

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ const router = Router();
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-
 const equipos= JSON.parse(fs.readFileSync('src/equipos.json', 'utf8'));
 const circuitos= JSON.parse(fs.readFileSync('src/circuitos.json', 'utf8'));
 const escuderia= JSON.parse(fs.readFileSync('src/equipos.json', 'utf8'));
@@ -11,8 +10,6 @@ const resultados = JSON.parse(fs.readFileSync('src/resultados.json', 'utf8'))
 // esto se hace como un recopilador del dato anterior va guardar los datos que tenia antes
 const json_resultado =fs.readFileSync('src/resultados.json','utf8');
 let carreras = JSON.parse(json_resultado);
-
-
 
 router.get('/',(req,res) => {
     res.render('index.ejs',{
@@ -27,7 +24,7 @@ router.get('/new-ingreso',(req,res)=>{
 
 //Configuro la ruta POST
 router.post('/new-ingreso',(req,res)=>{
-     console.log(req.body);
+    console.log(req.body);
     const {piloto,minutos,ubicacion,abandono,puntaje,circuitos,escuderia}=req.body;
     // Validamos los datos de la Carrera
     if(!piloto || !minutos || !ubicacion || !abandono|| !puntaje || !circuitos ||!escuderia){
@@ -63,7 +60,98 @@ router.get('/delete/:id',(req,res)=>{
     res.redirect('/');
 
 });
-router.get('/new-resultado',(req,res)=>{
-    res.render('resultados.ejs',{equipos,circuitos,escuderia,resultados});
+
+    router.get('/abandono', (req, res) => {
+        let abandonoPilotos = {};
+        // Loop through each race result
+        for (let carrera of carreras) {
+            let abandono = carrera.abandono.toUpperCase();
+            let piloto = carrera.piloto;
+          // If the driver abandoned the race, increment their count
+            if (abandono === "SI") {
+                if (abandonoPilotos[piloto]) {
+                    abandonoPilotos[piloto]++;
+                } else {
+                    abandonoPilotos[piloto] = 1;
+                }
+            }
+        }
+        let rankingAbandono = [];
+        // Convert the object to an array and sort by number of abandonments
+        for (let piloto in abandonoPilotos) {
+            rankingAbandono.push({ piloto: piloto, abandonos: abandonoPilotos[piloto] });
+        }
+        rankingAbandono.sort((a, b) => b.abandonos - a.abandonos);
+        res.render('abandono.ejs', { rankingAbandono });
 });
+//-------------------------------------------------------------------------------------------------------
+// REALIZA LA  SUMA PUNTAJES
+function calcularPuntajes(resultados) {
+    const puntajes = {};
+    resultados.forEach((resultado) => {
+        const escuderia = resultado.escuderia;
+        const puntos = parseInt(resultado.puntaje);
+        if (puntajes[escuderia]) {
+        puntajes[escuderia] += puntos;
+        } else {
+        puntajes[escuderia] = puntos;
+        }
+    });
+    return puntajes;
+}
+function ordenarPuntajes(puntajes) {
+    // El método Object.fromEntries clave y valor
+    const puntajesOrdenados = Object.fromEntries(
+        // ordena los puntajes a y b
+        Object.entries(puntajes).sort((a, b) => b[1] - a[1])
+    );
+    return puntajesOrdenados;
+}
+router.get('/puntajes', (req, res) => {
+    const puntajes = calcularPuntajes(resultados);
+    const puntajesOrdenados = ordenarPuntajes(puntajes);
+    res.render('puntajes.ejs', { puntajes: puntajesOrdenados });
+    });
+
+
+//tabla
+function calcularPuntaje(resultado) {
+    let posicion = parseInt(resultado.ubicacion);
+    if (posicion <= 10) {
+      switch (posicion) {
+        case 1:
+          return 25;
+        case 2:
+          return 18;
+        case 3:
+          return 15;
+        case 4:
+          return 12;
+        case 5:
+          return 10;
+        case 6:
+          return 8;
+        case 7:
+          return 6;
+        case 8:
+          return 4;
+        case 9:
+          return 2;
+        case 10:
+          return 1;
+      }
+    } else {
+      return 0;
+    }
+  }
+router.get('/new-resultados', (req, res) => {
+  let resultadosConPuntaje = resultados.map((resultado) => ({
+    ...resultado,
+    puntaje: calcularPuntaje(resultado),
+  }));
+
+  res.render('resultados.ejs', {resultadosConPuntaje,circuitos,resultados});
+});
+
 module.exports = router;
+
